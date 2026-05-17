09f9ee5cd2549bbdb6389a7569e4047eb62168d2
Vita927 <vita29.2007@gmail.com>
2026-05-17 10:46:46 +0300

    initial commit

---
+++ formatter_ex.cpp
@@ -0,0 +1,5 @@
+#include "formatter_ex.h"
+
+std::string format_ex(const std::string& message) {
+    return format("[ERROR] " + message);
+}
---
+++ formatter_ex.h
@@ -0,0 +1,9 @@
+#ifndef FORMATTER_EX_H
+#define FORMATTER_EX_H
+
+#include <string>
+#include "formatter.h"
+
+std::string format_ex(const std::string& message);
+
+#endif
